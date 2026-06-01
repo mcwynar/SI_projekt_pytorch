9b1ad31ec9f67ab36d9bebb0963266633c38677d
mcwynar <mcwynar04@gmail.com>
2026-06-01 11:50:10 +0200

    Dodanie eksperymentow

--- DL.py
+++ DL.py
@@ -24,10 +24,10 @@ if min(y_t.T)[0] > 0:
     y=y_t.squeeze()-1 #index of first class should equal to 0
 else:
     y=y_t.squeeze()
-    
+
 X=x.T
 
-# Scale data to have mean 0 and variance 1 
+# Scale data to have mean 0 and variance 1
 # which is importance for convergence of the neural network
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
@@ -48,7 +48,7 @@ class Model(nn.Module):
         self.layer1 = nn.Linear(input_dim, K1)
         self.layer2 = nn.Linear(K1, K2)
         self.layer3 = nn.Linear(K2, output_dim)
-        
+
     def forward(self, x):
         x = F.relu(self.layer1(x))
         x = F.relu(self.layer2(x))
@@ -61,43 +61,43 @@ K2_vec = K1_vec
 PK_2D_K1K2 = np.zeros([len(K1_vec),len(K2_vec)])
 max_epoch  = 100
 PK_2D_K1K2_max = 0
-k1_ind_max = 0 
+k1_ind_max = 0
 k2_ind_max = 0
 
 X_train = Variable(torch.from_numpy(X_train)).float()
 y_train = Variable(torch.from_numpy(y_train)).long()
 X_test  = Variable(torch.from_numpy(X_test)).float()
 y_test  = Variable(torch.from_numpy(y_test)).long()
-        
+
 for k1_ind in range(len(K1_vec)):
     for k2_ind in range(len(K2_vec)):
         model     = Model(X_train.shape[1], int(max(y)+1), K1_vec[k1_ind], K2_vec[k2_ind])
         optimizer = torch.optim.Adam(model.parameters(), lr=lr_vec[0])
         loss_fn   = nn.CrossEntropyLoss()
         # print(model)
-        
-        for epoch in range(max_epoch):    
+
+        for epoch in range(max_epoch):
             y_pred = model(X_train)
             loss = loss_fn(y_pred, y_train)
-            
+
             # Zero gradients
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            
+
         with torch.no_grad():
             y_pred = model(X_test)
             correct = (torch.argmax(y_pred, dim=1) == y_test).type(torch.FloatTensor)
             PK = correct.mean().item()*100
             print("K1 {} | K2 {} | PK {}". format(K1_vec[k1_ind], K2_vec[k2_ind], PK))
             PK_2D_K1K2[k1_ind, k2_ind] = PK
-            
+
         if PK > PK_2D_K1K2_max:
             PK_2D_K1K2_max = PK
-            k1_ind_max = k1_ind 
-            k2_ind_max = k2_ind         
-  
-            
+            k1_ind_max = k1_ind
+            k2_ind_max = k2_ind
+
+
 print("OPTYMALNE WARTOŚCI K1K2: K1={} | K2={} | lr={} | PK={}".\
       format(K1_vec[k1_ind_max], K2_vec[k2_ind_max], lr_vec[0], \
              PK_2D_K1K2[k1_ind_max, k2_ind_max]))
@@ -110,5 +110,5 @@ ax.set_xlabel('K1')
 ax.set_ylabel('K2')
 ax.set_zlabel('PK')
 
-ax.view_init(30, 200)   
-# plt.savefig("Fig.1_PK_K1K2_pytorch_iris.png",bbox_inches='tight')         
+ax.view_init(30, 200)
+plt.savefig("Fig.1_PK_K1K2_pytorch_iris.png",bbox_inches='tight')
